a81bdcbfb239f66f97411f6fca55832a5bff0554
Xaedaile <136849602+Xaedaile@users.noreply.github.com>
2023-07-30 16:02:24 -0400

    Add files via upload

---
+++ lab9.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// RecordType
+struct RecordType
+{
+	int		id;
+	char	name;
+	int		order;
+	struct RecordType *next;
+};
+
+// Fill out this structure
+struct HashType
+{
+	int numOfElements;
+	struct RecordType **arr;
+};
+
+// Compute the hash function
+int hash(int x)
+{
+
+}
+
+// parses input file to an integer array
+int parseData(char* inputFileName, struct RecordType** ppData)
+{
+	FILE* inFile = fopen(inputFileName, "r");
+	int dataSz = 0;
+	int i, n;
+	char c;
+	struct RecordType *pRecord;
+	*ppData = NULL;
+
+	if (inFile)
+	{
+		fscanf(inFile, "%d\n", &dataSz);
+		*ppData = (struct RecordType*) malloc(sizeof(struct RecordType) * dataSz);
+		// Implement parse data block
+		if (*ppData == NULL)
+		{
+			printf("Cannot allocate memory\n");
+			exit(-1);
+		}
+		for (i = 0; i < dataSz; ++i)
+		{
+			pRecord = *ppData + i;
+			fscanf(inFile, "%d ", &n);
+			pRecord->id = n;
+			fscanf(inFile, "%c ", &c);
+			pRecord->name = c;
+			fscanf(inFile, "%d ", &n);
+			pRecord->order = n;
+		}
+
+		fclose(inFile);
+	}
+
+	return dataSz;
+}
+
+// prints the records
+void printRecords(struct RecordType pData[], int dataSz)
+{
+	int i;
+	printf("\nRecords:\n");
+	for (i = 0; i < dataSz; ++i)
+	{
+		printf("\t%d %c %d\n", pData[i].id, pData[i].name, pData[i].order);
+	}
+	printf("\n\n");
+}
+
+// display records in the hash structure
+// skip the indices which are free
+// the output will be in the format:
+// index x -> id, name, order -> id, name, order ....
+void displayRecordsInHash(struct HashType *pHashArray, int hashSz)
+{
+	int i;
+
+	for (i=0;i<hashSz;++i)
+	{
+		// if index is occupied with any records, print all
+		if(pHashArray->arr[i] != NULL)
+		{
+			printf("index %d ", i);
+
+			while(pHashArray->arr[i] != NULL)
+			{
+				printf("-> %d, %s, %d ", pHashArray->arr[i]->id, pHashArray->arr[i]->name, pHashArray->arr[i]->order);
+				pHashArray->arr[i] = pHashArray->arr[i]->next;
+			}
+			printf("\n");
+		}
+	}
+}
+
+int main(void)
+{
+	struct RecordType *pRecords;
+	int recordSz = 0;
+
+	recordSz = parseData("input.txt", &pRecords);
+	printRecords(pRecords, recordSz);
+
+	// Your hash implementation
+	struct HashType *hmap = (struct HashType*) malloc(sizeof(struct HashType));
+	hmap->numOfElements = recordSz;
+	
+	for(int i=0; i<recordSz; i++)
+	{
+		int j = pRecords[i].order;
+		
+		if(hmap->arr[j] == NULL)
+			*hmap->arr[j] = pRecords[i];
+		else
+		{
+			while(hmap->arr[j]->next != NULL)
+				hmap->arr[j] = hmap->arr[j]->next;
+
+			*hmap->arr[j]->next = pRecords[i];
+		}
+	}
+	return 0;
+}
